3ac1fa66436a60b36092b592d63cf4e8d0ed30d9
LucasGoianaMalicia <lucasgoianam@hotmail.com>
2022-07-03 22:55:45 -0300

    Botao

--- app/src/main/java/br/com/fiap/affily/ChildrenViewHolder.kt
+++ app/src/main/java/br/com/fiap/affily/ChildrenViewHolder.kt
@@ -1,15 +1,20 @@
 package br.com.fiap.affily
 
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.fiap.affily.models.entities.Children
 
 class ChildrenViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
     private val nameText: TextView = itemView.findViewById(R.id.tvProduct)
+    private val deleteButton: TextView = itemView.findViewById(R.id.excluir)
+
     fun bindItem(children: Children) {
         itemView.apply {
             nameText.text = children.nome
+            deleteButton.text = "Deletar "
+
         }
     }
 }
